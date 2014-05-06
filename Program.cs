db3231a3d227169f1eb2288b10476afa4649f956
Andrew William Krause <andrewkrause75@roadrunner.com>
2014-05-06 12:42:38 -0400

    initial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestProductInASeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
